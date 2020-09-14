2d0c92d78152899b5b2e178a80621d7d441ff311
Graham Watson <graham@phantomwatson.com>
2020-09-14 16:32:40 -0400

    Refactor

--- templates/element/analytics.php
+++ templates/element/analytics.php
@@ -3,23 +3,20 @@
     use Cake\Core\Configure;
 
     $googleAnalyticsId = Configure::read('google_analytics_id');
-    $debug = Configure::read('debug');
     $gaConfig = [
         'page_location' => $this->request->getUri()->__toString(),
-        'page_path' => $this->request->getUri()->getPath()
+        'page_path' => $this->request->getUri()->getPath(),
+        'page_title' => $titleForLayout ?? null,
     ];
-    if (isset($titleForLayout) && $titleForLayout) {
-        $gaConfig['page_title'] = $titleForLayout;
-    }
 ?>
-<?php if ($googleAnalyticsId && !$debug): ?>
+<?php if ($googleAnalyticsId && !Configure::read('debug')): ?>
     <!-- Global Site Tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?= $googleAnalyticsId ?>"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', '<?= $googleAnalyticsId ?>', <?= json_encode($gaConfig) ?>);
+        gtag('config', <?= json_encode($googleAnalyticsId) ?>, <?= json_encode($gaConfig) ?>);
         gtag('event', 'page_view');
     </script>
 <?php endif; ?>
