c28bd8f19bb60fc3c5d7c4beb985a94afdcf1ea4
Graham Watson <graham@phantomwatson.com>
2020-09-17 17:23:10 -0400

    Update flash message elements to use Bootstrap styles

--- templates/element/flash/info.php
+++ templates/element/flash/info.php
@@ -8,7 +8,7 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
+<div class="alert alert-info alert-dismissible fade show" role="alert">
     <?= $message ?>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
